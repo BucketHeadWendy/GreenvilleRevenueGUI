08d5c752751fb42d7aecbdac3b95b42cca6e4bd9
Derek West <derek.west@outlook.com>
2017-01-28 12:27:25 -0600

    First Draft

--- GreenvilleRevenueGUI/Form1.Designer.cs
+++ GreenvilleRevenueGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GreenvilleRevenueGUI
 {
-    partial class Form1
+    partial class GreenvilleGUI
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lastLabel = new System.Windows.Forms.Label();
+            this.thisLabel = new System.Windows.Forms.Label();
+            this.lastYearBox = new System.Windows.Forms.Label();
+            this.thisYearBox = new System.Windows.Forms.Label();
+            this.CalcNow = new System.Windows.Forms.Button();
+            this.revenueBox = new System.Windows.Forms.Label();
+            this.lastBox = new System.Windows.Forms.TextBox();
+            this.thisBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lastLabel
+            // 
+            this.lastLabel.AutoSize = true;
+            this.lastLabel.Location = new System.Drawing.Point(13, 61);
+            this.lastLabel.Name = "lastLabel";
+            this.lastLabel.Size = new System.Drawing.Size(281, 13);
+            this.lastLabel.TabIndex = 0;
+            this.lastLabel.Text = "Enter the number of contestants in last year\'s competition: ";
+            // 
+            // thisLabel
+            // 
+            this.thisLabel.AutoSize = true;
+            this.thisLabel.Location = new System.Drawing.Point(13, 113);
+            this.thisLabel.Name = "thisLabel";
+            this.thisLabel.Size = new System.Drawing.Size(281, 13);
+            this.thisLabel.TabIndex = 1;
+            this.thisLabel.Text = "Enter the number of contestants in this year\'s competition: ";
+            // 
+            // lastYearBox
+            // 
+            this.lastYearBox.AutoSize = true;
+            this.lastYearBox.Location = new System.Drawing.Point(13, 217);
+            this.lastYearBox.Name = "lastYearBox";
+            this.lastYearBox.Size = new System.Drawing.Size(0, 13);
+            this.lastYearBox.TabIndex = 2;
+            // 
+            // thisYearBox
+            // 
+            this.thisYearBox.AutoSize = true;
+            this.thisYearBox.Location = new System.Drawing.Point(13, 265);
+            this.thisYearBox.Name = "thisYearBox";
+            this.thisYearBox.Size = new System.Drawing.Size(0, 13);
+            this.thisYearBox.TabIndex = 3;
+            // 
+            // CalcNow
+            // 
+            this.CalcNow.Location = new System.Drawing.Point(243, 169);
+            this.CalcNow.Name = "CalcNow";
+            this.CalcNow.Size = new System.Drawing.Size(134, 23);
+            this.CalcNow.TabIndex = 4;
+            this.CalcNow.Text = "Click to Calculate";
+            this.CalcNow.UseVisualStyleBackColor = true;
+            this.CalcNow.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // revenueBox
+            // 
+            this.revenueBox.AutoSize = true;
+            this.revenueBox.Location = new System.Drawing.Point(16, 308);
+            this.revenueBox.Name = "revenueBox";
+            this.revenueBox.Size = new System.Drawing.Size(0, 13);
+            this.revenueBox.TabIndex = 5;
+            // 
+            // lastBox
+            // 
+            this.lastBox.Location = new System.Drawing.Point(301, 53);
+            this.lastBox.Name = "lastBox";
+            this.lastBox.Size = new System.Drawing.Size(100, 20);
+            this.lastBox.TabIndex = 6;
+            // 
+            // thisBox
+            // 
+            this.thisBox.Location = new System.Drawing.Point(301, 105);
+            this.thisBox.Name = "thisBox";
+            this.thisBox.Size = new System.Drawing.Size(100, 20);
+            this.thisBox.TabIndex = 7;
+            // 
+            // GreenvilleGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(631, 395);
+            this.Controls.Add(this.thisBox);
+            this.Controls.Add(this.lastBox);
+            this.Controls.Add(this.revenueBox);
+            this.Controls.Add(this.CalcNow);
+            this.Controls.Add(this.thisYearBox);
+            this.Controls.Add(this.lastYearBox);
+            this.Controls.Add(this.thisLabel);
+            this.Controls.Add(this.lastLabel);
+            this.Name = "GreenvilleGUI";
+            this.Text = "Greenville Revenue Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lastLabel;
+        private System.Windows.Forms.Label thisLabel;
+        private System.Windows.Forms.Label lastYearBox;
+        private System.Windows.Forms.Label thisYearBox;
+        private System.Windows.Forms.Button CalcNow;
+        private System.Windows.Forms.Label revenueBox;
+        private System.Windows.Forms.TextBox lastBox;
+        private System.Windows.Forms.TextBox thisBox;
     }
 }
 
--- GreenvilleRevenueGUI/Form1.cs
+++ GreenvilleRevenueGUI/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Derek West & Dan Plummer
+//1/26/2017
+//Chapter 3, Case Problem 1
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +14,30 @@ using System.Windows.Forms;
 
 namespace GreenvilleRevenueGUI
 {
-    public partial class Form1 : Form
+    public partial class GreenvilleGUI : Form
     {
-        public Form1()
+        public GreenvilleGUI()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int lastYear, thisYear, admission, currentRevenue;
+            
+
+            lastYear = Convert.ToInt32(lastBox.Text);
+            thisYear = Convert.ToInt32(thisBox.Text);
+            admission = 25;
+            currentRevenue = admission * thisYear;
+
+            bool moreThisYear = thisYear > lastYear;
+
+            lastYearBox.Text = "There were " + lastYear + " contestants last year, and " + thisYear + " constestants this year.";
+            thisYearBox.Text = "To say that there were more contestants this year than last year would be: " + moreThisYear;
+            revenueBox.Text = "Expected revenue for this year's competition is: $" + currentRevenue;
+          
+
         }
     }
 }
